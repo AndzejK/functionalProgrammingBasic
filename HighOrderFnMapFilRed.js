94c836111a8ed5797995ef2abe3dbdecfc8b3987
MAC <andrejkrupoves@gmail.com>
2021-03-14 09:28:08 +1100

    practising functional prog
    a good reminder how to use the loginal NOT

--- HighOrderFnMapFilRed.js
+++ HighOrderFnMapFilRed.js
@@ -7,9 +7,8 @@ and reduce() so that it returns a new array containing only the square of only t
 const squareList = arr => {
     // #1 the only positive number and can find it by using comparison operators ">""<";
     // #2 just whole a number from 0 till 9, for instance, 4.8 we should skip it, so far so don't know how to seperate it;
-    // #3 just signle integer not decimal and higher nominal, for example, 10 or 111 skip them so more than <10;
     let positiveIntegersArr=arr.filter(x=>{
-        return(x>=0&&!(x%1)) //Might this point is important to solve this task. 
+        return(x>=0&&!(x%1)) //Might be this point is important to solve this task. 
         });
        
     return positiveIntegersArr.map(y=>y*y);
