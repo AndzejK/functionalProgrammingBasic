298e0daebe8a1b4c2e0bdf09798db8dade584cfd
MAC <andrejkrupoves@gmail.com>
2021-03-16 09:17:07 +1100

    solving more task relating to functional program

--- joinMethod.js
+++ joinMethod.js
@@ -1,3 +1,11 @@
+function autohyphen(resultFromPreviousFn){
+    let start="";
+    for(let i=0;i<resultFromPreviousFn.length;i++){
+        start+="-";
+    }
+    console.log(start);
+}
+
 /*
 source: freecodecamp.org
 The join method is used to join the elements of an array together to create a string. 
@@ -12,5 +20,24 @@ function sentensify(str){
     result=sentesifyArr.join(" ");
     return result;
 }
-let brokenStr = "The.force.is.strong.with.this.one";
+let brokenStr = "The.force.is.strong.with.this.one.test.test1";
 console.log(sentensify(brokenStr));
+autohyphen(sentensify(brokenStr));
+
+/*
+Task #1 - source: freecodecamp
+Many content management sites (CMS) have the titles of a post added to part of the URL 
+for simple bookmarking purposes. For example, if you write a Medium post titled Stop Using Reduce, 
+it's likely the URL would have some form of the title string in it (.../stop-using-reduce). 
+*/
+
+let title=" Winter Is  Coming ";
+
+function urlSlug(tit) {
+    let seperateWord=[];
+    seperateWord=tit.match(/\w+/gm);
+   return seperateWord.join("-").toLowerCase();
+    
+}
+console.log(urlSlug(title));
+autohyphen(urlSlug(title));
